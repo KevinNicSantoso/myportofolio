ea99b45b0807f1451f00c991643ffbc45752fb1a
KevinNicSantoso <kevin.nicholas51@ui.ac.id>
2026-09-13 15:59:41 +0700

    Fixed PWS Domain

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -19,7 +19,7 @@ from dotenv import load_dotenv
 # Load environment variables from .env file
 load_dotenv()
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "kevin-nicholas51-myportofolio.pws.cs.ui.ac.id"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "kevin-nicholas51-myportofolio.pws.cs.ui.ac.id",]
 
 PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 
